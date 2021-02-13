09578489b763cf6cfc9b67e453769cccd3425376
J-Boogie <jackr353@gmail.com>
2021-02-13 12:28:05 -0600

    get burger seeds from samya and modify table to make fit

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
 ### Schema
 
+DROP DATABASE IF EXISTS boiga_db;
 CREATE DATABASE boiga_db;
 USE boiga_db;
 
 CREATE TABLE boigas (
     id iNT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+### SEEDS ARE FROM SAMYA
+
+INSERT INTO boigas (id, burger_name, devoured)
+VALUES (1, "California Avocado Burger", true), (3, "Chilli Burger", true), (5, "Salmon Burger", true), (7, "Hamburger", true), (8, "Veggie Burger", true),
+(2, "Cheese Burger", false), (4, "Oklahoma Onion Burger", false), (6, "Steak Burger", false);
