7316a45f5cba7ed0d0d783c677c8ede905413f97
J-Boogie <jackr353@gmail.com>
2021-02-20 13:18:47 -0600

    final touches

--- config/connection.js
+++ config/connection.js
@@ -14,14 +14,6 @@ if (process.env.JAWSDB_URL) {
     })
 }
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: pw,
-    database: 'boiga_db'
-});
-
 // Make Connection
 connection.connect((err) => {
     if (err) {
